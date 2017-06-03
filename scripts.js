81216b470cd65097eaa0352755ef429f33f84ebd
Aubrey Masten <mastenaub@gmail.com>
2017-06-03 14:12:50 -0700

    separate centroid and dataset generation

--- scripts.js
+++ scripts.js
@@ -22,6 +22,12 @@ function Vector(params){
   this.color = params.color;
 }
 
+function Centroid(params) {
+  this.x = params.x;
+  this.y = params.y;
+  this.color = params.color;
+}
+
 function Dataset(params){
   this.max = params.max;
   this.min = params.min;
@@ -30,14 +36,14 @@ function Dataset(params){
   this.rand = new Random();
 }
 
-Dataset.prototype.generateArbitrary = function(count, destination) {
+Dataset.prototype.generateArbitrary = function(count) {
   for(let i = 0; i < count; i++){
-    let point = new Vector({
+    let vector = new Vector({
       x: this.rand.int(this.min, this.max),
       y: this.rand.int(this.min, this.max),
       color: 'white'
     });
-    destination.push(point);
+    this.vectors.push(vector);
   }
 }
 
@@ -45,21 +51,22 @@ Dataset.prototype.display = function(){
   this.vectors.forEach(function(v){
     Shape.Circle(v.x,v.y,2).fillColor = `${v.color}`;
   });
+
   this.centroids.forEach(function(c){
     Shape.Rectangle(c.x,c.y,5,5).fillColor = `${c.color}`;
   });
 }
 
 Dataset.prototype.generateCentroids = function(centroidCount){
-  this.generateArbitrary(centroidCount, this.centroids);
-  this.assignCentroidColor(this.centroids);
-}
-
-Dataset.prototype.assignCentroidColor = function(vectors){
-  vectors.forEach(function(v){
+  for(let i = 0; i < centroidCount; i++){
     let c = this.rand.color();
-    v.color = `rgb(${c.r},${c.g},${c.b})`;
-  }, this);
+    let centroid = new Centroid({
+      x: this.rand.int(this.min, this.max),
+      y: this.rand.int(this.min, this.max),
+      color: `rgb(${c.r},${c.g},${c.b})`
+    });
+    this.centroids.push(centroid);
+  }
 }
 
 Dataset.prototype.kMeans = function(params){
@@ -75,13 +82,13 @@ $(document).ready(function(){
   displayInit({canvas: 'canvas'})
   let data = new Dataset({min: 0, max: 600})
 
-  $('#gen').click(function(){
-    data.generateArbitrary(20, data.vectors);
+  $('#gen-arb').click(function(){
+    data.generateArbitrary(20);
     data.display();
   });
-  $('#k-means').click(function(){
-    data.kMeans({centroids: 5, iterations: 20});
+  $('#gen-cen').click(function(){
+    data.generateCentroids(5);
     data.display();
-  })
+  });
   paper.view.draw();
 });
